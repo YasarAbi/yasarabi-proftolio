61951683d7490d6524b65aec3c30f09fed1c2b07
YasarAbi <73652808+YasarAbi@users.noreply.github.com>
2022-04-22 11:26:23 +0300

    Footer hazırlandı.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {SiDiscord} from 'react-icons/si'
 
 const footer = () => {
   return (
-    <section>footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>Yaşar Karaali</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Anasayfa</a></li>
+        <li><a href="#hakkimda">Hakkımda</a></li>
+        <li><a href="#deneyimler">Deneyimlerim</a></li>
+        <li><a href="#servisler">Servislerim</a></li>
+        <li><a href="#portfolyo">Anasayfa</a></li>
+        <li><a href="#iletisim">İletişim</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://linkedin.com/" target='_blank'><BsLinkedin /></a>
+        <a href="https://github.com/" target='_blank'><BsGithub /></a>
+        <a href="https://discord.com/" target='_blank'><SiDiscord /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Yaşar Karaali tarafından hazırlanmış başlangıç seviye ReactJS uygulaması.</small>
+      </div>
+
+    </footer>
   )
 }
 
